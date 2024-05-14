c7914b84d28c71a21abedce5444272a27cd66a30
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-05-15 00:40:10 +0530

    firstMain

---
+++ public/index.js
@@ -0,0 +1,71 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+//menu bar
+burger.addEventListener('click', () => {
+navLinks.classList.toggle('nav-active');
+});
+
+//nav bar pin
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+var counter = 0;
+
+slides.forEach(
+    (slide,index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+//slide buttons
+const goNext = () =>{
+    counter++
+    if (counter === slides.length) {
+        counter = 0;
+    }
+    slideImage() 
+}
+const goPrev = () =>{
+    counter--
+    if (counter < 0) {
+        counter = slides.length - 1;
+    }
+    slideImage() 
+}
+
+//image slide
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
+
+//slide intervel
+const intervalId = setInterval(() => {
+    counter++;
+    if (counter === slides.length) {
+        counter = 0;
+    }
+    slideImage();
+}, 10000);
+
+prevBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+});
+nextBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+});
+
+
